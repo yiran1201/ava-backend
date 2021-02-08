d4e555b07158da6a9e2eaf5ecc1251ceefa2d1f5
yiran1201 <yiranchen1201@gmail.com>
2021-02-07 21:52:50 -0800

    update deploy

--- utils/socket-util.js
+++ utils/socket-util.js
@@ -6,17 +6,17 @@ export const connectSocket = (server) => {
   io.on('connection', (socket) => {
     console.log('Client connected!');
 
-    socket.on('syncConversations', () => {
+    socket.on('syncConversationsOnServer', () => {
       // broadcast to all users
-      io.emit('retrieveConversations', db.getConversations());
+      io.emit('syncConversationsOnClient', db.getConversations());
     });
 
-    socket.on('syncConversation', (conversationId) => {
-      io.emit('updateConversation', db.getConversation(conversationId));
+    socket.on('syncConversationOnServer', (conversationId) => {
+      io.emit('syncConversationOnClient', db.getConversation(conversationId));
     });
 
-    socket.on('deleteConversation', (conversationId) => {
-      io.emit('clearConversation', conversationId);
+    socket.on('deleteConversationOnServer', (conversationId) => {
+      io.emit('deleteConversationOnClient', conversationId);
     });
   });
 
